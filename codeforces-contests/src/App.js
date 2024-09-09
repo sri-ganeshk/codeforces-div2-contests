92dc73b2a11e663e974c38d1943a9feb06dcf3cd
sriganesh412 <116094202+sriganesh412@users.noreply.github.com>
2024-09-09 11:48:57 +0000

    Describe your changes here

---
+++ codeforces-contests/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContestList from './ContestList';
+
+function App() {
+  return (
+    <div className="App">
+      <ContestList />
+    </div>
+  );
+}
+
+export default App;
